65ee45573edc227eb98431a9a5116d6898f47cd6
HARSHANAND00 <81462084+HARSHANAND00@users.noreply.github.com>
2023-10-29 22:39:43 +0530

    click update

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -4,14 +4,15 @@ import { Button } from "@mui/material";
 
 function EventCard({ eventname, image, link }) {
   return (
-    <a href={link}><div className="EventsContainer">
-      <img src={image} onClick={link} />
+    <div className="EventsContainer">
+      <img src={image} onClick={()=>{
+         window.location=link;
+      }} />
       <div className="EventTitle text-center font-['Title']"  >
         <Button variant="contained"><a href={link}><h3 className="text-1xl text-white">Register</h3></a></Button>
       </div>
       
     </div>
-    </a>
   );
 }
 
