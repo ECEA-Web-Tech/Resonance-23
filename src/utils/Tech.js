0ec564242e25747542e4bee074b835d318311290
Deepak Vasan R <rkdldedeepak@gmail.com>
2023-10-30 16:08:59 +0530

    Added link to project display

--- src/utils/Tech.js
+++ src/utils/Tech.js
@@ -3,28 +3,31 @@ import CodeRoyale from "../assets/images/posters/Design Posters/Tech/code royale
 import Mindcraft from "../assets/images/posters/Design Posters/Tech/Mindcraftai.png";
 import ProjectDisplay from "../assets/images/posters/Design Posters/Tech/PROJECT DISPLAY RESONANCE.jpg";
 
-
 const Tech = [
-    {
-        eventname: "Circuit Mystiques",
-        image: CircuitMysiques,
-        link: "https://docs.google.com/forms/d/e/1FAIpQLSeAR8T6hCoVowytOskURA8-yi2cUhFwDMbOmZq2PfbQNT2Zuw/viewform?usp=sf_link"
-    },
-    {
-        eventname: "Code Royale",
-        image: CodeRoyale,
-        link: "https://docs.google.com/forms/d/e/1FAIpQLSeGRWgpdAXqABwAP588awQ1BGaK7TRHi6kZ6AYGGyt2AtkQFg/viewform?usp=sf_link"
-    },
-    {
-        eventname: "Mind Craft",
-        image: Mindcraft,
-        link: "https://docs.google.com/forms/d/e/1FAIpQLSdfYEg1owPVMmdEf5yK2YmU2MQ93eHclf8Pm0YUigBivF2hLQ/viewform?usp=sf_link"
-    },
-    {
-        eventname: "Project Display",
-        image: ProjectDisplay,
-        link: ""
-    }
-]
+  {
+    eventname: "Circuit Mystiques",
+    image: CircuitMysiques,
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeAR8T6hCoVowytOskURA8-yi2cUhFwDMbOmZq2PfbQNT2Zuw/viewform?usp=sf_link",
+  },
+  {
+    eventname: "Code Royale",
+    image: CodeRoyale,
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeGRWgpdAXqABwAP588awQ1BGaK7TRHi6kZ6AYGGyt2AtkQFg/viewform?usp=sf_link",
+  },
+  {
+    eventname: "Mind Craft",
+    image: Mindcraft,
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLSdfYEg1owPVMmdEf5yK2YmU2MQ93eHclf8Pm0YUigBivF2hLQ/viewform?usp=sf_link",
+  },
+  {
+    eventname: "Project Display",
+    image: ProjectDisplay,
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLSeFUAcEkG7Hrle5axaJ1RKdvapz9gHXiMXoOcRgniSp3wnaDg/viewform?usp=pp_url",
+  },
+];
 
 export default Tech;
