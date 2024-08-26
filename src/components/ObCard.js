54c557d68a5acfd84b557ec36c73f3a5daf08f83
Abinaya103 <abinayaboopathy77@gmail.com>
2024-08-26 14:29:14 +0530

    Ob details update

--- src/components/ObCard.js
+++ src/components/ObCard.js
@@ -9,7 +9,7 @@ function ObCard({ name, designation, photo,instagram,linkedin }) {
     <div className="Container">
       <img src={photo} />
       <div className="Title text-center font-['Title'] ">
-        <h2 className="text-2xl mb-2">{name}</h2>
+        <h2 style={{ fontSize: '22px' }} className="mb-2">{name}</h2>
         <h3 className="text-1xl text-gray-400">{designation}</h3>
         <div>
         <Button href={instagram}>
--- src/utils/ObDetails.js
+++ src/utils/ObDetails.js
@@ -1,53 +1,63 @@
-import dharshin from "../assets/images/officeBearers/dharshin.jpg";
-import siddarth from "../assets/images/officeBearers/siddarth.jpg";
-import yaazhini from "../assets/images/officeBearers/yaazhini.jpg";
-import ajay from "../assets/images/officeBearers/ajay.jpg";
-import priya from "../assets/images/officeBearers/priya.jpg";
-import sai from "../assets/images/officeBearers/sai.jpeg";
-import tamil from "../assets/images/officeBearers/tamilselvan.JPG";
-import mayuri from "../assets/images/officeBearers/mayuri.jpg";
+import balaji from "../assets/images/officeBearers/balaji.jpeg";
+import harsh from "../assets/images/officeBearers/harsh.jpg";
+import jenifer from "../assets/images/officeBearers/jeni.jpg";
+import lakshanya from "../assets/images/officeBearers/lakshanya.jpg";
+import sivakumar from "../assets/images/officeBearers/sivakumar.jpg";
+import suriya from "../assets/images/officeBearers/suriya.jpg";
+import vishwajeith from "../assets/images/officeBearers/vishwajeith.jpg";
+
 
 const ObDetails = [
   {
-    name: "Dharshin B",
+    name: "Harsh Kumar Doshi H",
     designation: "President",
-    photo: dharshin,
+    photo: harsh,
+    instagram:"https://www.instagram.com/harshdoshi_14?igsh=MWEzYTczNWFqOGtveg==",
+    linkedin:"https://in.linkedin.com/in/harsh-kumar-doshi"
   },
   {
-    name: "Siddharth K",
+    name: "Siva Kumar R V",
     designation: "Vice-President",
-    photo: siddarth,
+    photo: sivakumar,
+    instagram:"https://www.instagram.com/plug_in_the_swag?igsh=dWk4c3ZmMGJ4azAy",
+    linkedin:"https://www.linkedin.com/in/siva-rv-ece"
   },
   {
-    name: "Yaazhini A",
+    name: "Lakshanya R",
     designation: "Vice-President",
-    photo: yaazhini,
+    photo: lakshanya,
+    instagram:"https://www.instagram.com/lakshanya_05?igsh=MXFiZXMxZHF4emd0",
+    linkedin:"https://www.linkedin.com/in/lakshanya-ramachandran"
   },
   {
-    name: "Sai Kumar",
+    name: "S Vishwajeith",
     designation: "Deputy Treasurer",
-    photo: sai,
+    photo: vishwajeith,
+    instagram:"https://www.instagram.com/vishwandering_mind?igsh=c2J2OHBqZDI0a28y",
+    linkedin:"https://www.linkedin.com/in/vishwajeith-s-7bba19243?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
   },
   {
-    name: "Ajay Issac S",
+    name: "Suriya Prakash P",
     designation: "General Seceratry",
-    photo: ajay,
+    photo: suriya,
+    instagram:"https://www.instagram.com/suriya_.7/profilecard/?igsh=eWMxdjZ3NXlxeTVy",
+    linkedin:"https://www.linkedin.com/in/suriyaprakash7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
   },
   {
-    name: "Priya Rajammal",
+    name: "Jenifer B",
     designation: "General Seceratry",
-    photo: priya,
+    photo: jenifer,
+    instagram:"https://www.instagram.com/_jeni18_/",
+    linkedin:"https://www.linkedin.com/in/jenifer-b-305636248/"
   },
   {
-    name: "Tamil Selvan H",
+    name: "Balaji Prithviraj N",
     designation: "Genereal Secretary - PG",
-    photo: tamil,
+    photo: balaji,
+    instagram:"",
+    linkedin:"https://www.linkedin.com/in/balaji-prithviraj-4a55a21b8?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3VbESechTs60uPISyW%2B3AQ%3D%3D"
   },
-  {
-    name: "Mayuri M",
-    designation: "General Secretary - PG",
-    photo: mayuri,
-  },
+  
 ];
 
 export default ObDetails;
