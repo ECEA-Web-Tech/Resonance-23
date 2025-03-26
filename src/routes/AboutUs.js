7dbc0ff3c53ce59da1b2a17a8addba4e5dafda16
SARAVANAN <71633541+SARAVANAN532@users.noreply.github.com>
2025-03-26 14:16:45 +0530

    new

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -28,8 +28,8 @@ function AboutUs() {
 
       <Zoom triggerOnce={true} duration={800}>
         <Container
-          className="rounded-xl mt-36 mb-36 py-20 max-w-4xl shadow-lg  "
-          style={{ backgroundColor: "#9df6fa" }} // Darker, semi-transparent background
+          className="rounded-xl p-10 max-w-4xl shadow-lg mt-8 "
+          style={{ backgroundColor: "#30363a" }} // Darker, semi-transparent background
         >
           <div className="m-2 px-10 flex flex-col sm:flex-row justify-center items-center md:gap-32 sm:gap-20 my-3 sm:my-20">
             <Slide
@@ -75,7 +75,7 @@ function AboutUs() {
               delay={1400}
               duration={1200}
             >
-              <p className="text-black text-justify text-md md:text-lg lg:text-xl leading-relaxed">
+              <p className="text-white text-justify text-md md:text-lg lg:text-xl leading-relaxed">
                 <Typography
                   sx={{
                     fontSize: {
